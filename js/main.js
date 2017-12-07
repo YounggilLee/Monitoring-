73e837fb300b88e04b88034e4efca76a53d2f8b7
GiL <iamleeyounggil@gmail.com>
2017-12-07 10:55:29 -0500

    Fix the issue and implement websocket listener

--- js/main.js
+++ js/main.js
@@ -1,13 +1,21 @@
-
 $(document).ready(function() {
 //console.log(_.partition([1, 2, 3, 4], n => n % 2));
 
-var ws = new WebSocket('ws://138.197.146.172:8080/ws','wamp.2.json');
-ws.onopen = function () {
-console.log('websocket is connected ...')
-ws.send('Thank you for accepting this Web socket')
-}
-ws.onmessage = function (ev) {
-console.log(ev);
+var connection = new autobahn.Connection({
+    url : 'ws://138.197.146.172:8080/ws',
+    realm: 'realm1'
+    });
+    
+    connection.onopen = function (session) {
+    
+    console.log('websocket is connected ...')
+    
+    session.subscribe('com.test.temp', function(message) {
+    
+    console.log(message);
+    });
     }
+    
+    connection.open();
+
 }); // End ready function
